c64f4c9c6918086a7a07927967685a63279e6952
Celesse Aisle Nacpil <notaisle@gmail.com>
2026-03-11 13:21:52 +0800

    lab-act1

--- nacpil-client/src/App.jsx
+++ nacpil-client/src/App.jsx
@@ -4,32 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React App!</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Celesse Aisle M. Nacpil <br />
+          Email: notaisle@gmail.com <br />
+          GitHub: <a href="https://www.github.com/inthehallways" target="_blank" rel="noopener noreferrer">inthehallways</a> <br />
+          Fun fact: i like frogs xDDDD 
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </header>
+    </div>
+  );
 }
 
 export default App
