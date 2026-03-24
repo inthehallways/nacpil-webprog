841f8fa1a66acdd5cdac45be4ae8ea989b44fcd6
Celesse Aisle Nacpil <notaisle@gmail.com>
2026-03-25 05:00:15 +0800

    lab-act2: initial template implementation

--- nacpil-client/src/App.jsx
+++ nacpil-client/src/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//homepage structure
+import Layout from './components/Layout';
+import ArticlePage from './pages/ArticlePage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+const routes = [
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />
+      },
+      {
+        path: 'about',
+        element: <AboutPage />
+      },
+      {
+        path: 'articles',
+        element: <ArticlePage />
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to my React App!</h1>
-        <p>
-          Name: Celesse Aisle M. Nacpil <br />
-          Email: notaisle@gmail.com <br />
-          Section: INF231 <br />
-          GitHub: <a href="https://www.github.com/inthehallways" target="_blank" rel="noopener noreferrer">inthehallways</a> <br />
-          Fun fact: i like frogs xDDDD 
-        </p>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
